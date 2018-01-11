05d66fdb51d940a10186acd59fca394b586b26fc
Alex_Ice <alex_ice@github.com>
2018-01-11 11:09:18 +0000

    Ajout B_Ubble
    v0

---
+++ BUbble/app/src/main/java/com/example/alexi/b_ubble/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.alexi.b_ubble;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //React to button tap
+    public void didTapButton(View view) {
+        Button button = (Button)findViewById(R.id.button);
+        final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        button.startAnimation(myAnim);
+
+        // Use bounce interpolator with amplitude 0.2 and frequency 20
+        MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
+        myAnim.setInterpolator(interpolator);
+
+        button.startAnimation(myAnim);
+    }
+
+
+}
+
