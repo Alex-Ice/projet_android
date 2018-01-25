1cdeac52a2dec7b40c488bedaefe5e3bddf0b4a8
Alex_Ice <alex_ice@github.com>
2018-01-25 09:37:13 +0000

    COucou

--- BUbble/app/src/main/java/com/example/alexi/b_ubble/sea.java
+++ BUbble/app/src/main/java/com/example/alexi/b_ubble/sea.java
@@ -1,15 +1,9 @@
 package com.example.alexi.b_ubble;
 
-import android.content.Intent;
 import android.content.res.TypedArray;
-import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
 
 import com.igalata.bubblepicker.BubblePickerListener;
 import com.igalata.bubblepicker.adapter.BubblePickerAdapter;
@@ -20,22 +14,21 @@ import com.igalata.bubblepicker.rendering.BubblePicker;
 import org.jetbrains.annotations.NotNull;
 
 
-public class MainActivity extends AppCompatActivity {
-    
-    private BubblePicker picker1;
+public class sea extends AppCompatActivity {
+
+    private BubblePicker picker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sea);
+        picker = findViewById(R.id.picker);
 
-        picker1 = findViewById(R.id.picker1);
-
-        final String[] titles = {"Bienvenue"};
+        final String[] titles = getResources().getStringArray(R.array.planets_array);
         final TypedArray colors = getResources().obtainTypedArray(R.array.colors);
-        //final TypedArray images = getResources().obtainTypedArray(R.array.images);
+       //final TypedArray images = getResources().obtainTypedArray(R.array.images);
 
-        picker1.setAdapter(new BubblePickerAdapter() {
+        picker.setAdapter(new BubblePickerAdapter() {
             @Override
             public int getTotalCount() {
                 return titles.length;
@@ -49,26 +42,18 @@ public class MainActivity extends AppCompatActivity {
                 item.setGradient(new BubbleGradient(colors.getColor((position * 2) % 8, 0),
                         colors.getColor((position * 2) % 8 + 1, 0), BubbleGradient.VERTICAL));
                 //item.setTypeface(mediumTypeface);
-                item.setTextColor(ContextCompat.getColor(MainActivity.this.getApplicationContext(), android.R.color.white));
+                item.setTextColor(ContextCompat.getColor(sea.this.getApplicationContext(), android.R.color.white));
                 //item.setBackgroundImage(ContextCompat.getDrawable(DemoActivity.this, images.getResourceId(position, 0)));
                 return item;
             }
         });
 
-        picker1.setListener(new BubblePickerListener() {
+        //To get all selected items picker.getSelectedItems() method in Java.
+
+        picker.setListener(new BubblePickerListener() {
             @Override
             public void onBubbleSelected(@NotNull PickerItem item) {
-                /*Bubble bounce animation*/
-                final Animation myAnim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.bounce);
-                picker1.startAnimation(myAnim);
 
-                // Use bounce interpolator with amplitude 0.2 and frequency 20
-                MyBounceInterpolator interpolator = new MyBounceInterpolator(0.2, 20);
-                myAnim.setInterpolator(interpolator);
-
-                picker1.startAnimation(myAnim);
-                Intent intent = new Intent(MainActivity.this, sea.class);
-                new CounterTask().execute(intent);
             }
 
             @Override
@@ -78,35 +63,20 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class CounterTask extends AsyncTask<Intent, Void, Intent> {
-        @Override
-        protected Intent doInBackground(Intent... intents) {
-            int a = 0;
-            while (a != 200000000) {
-                a++;
-            } //environ 2 secondes
-            return intents[0];
-        }
-
-        protected void onPostExecute(Intent result) {
-            startActivity(result);
-        }
-    }
-
-
     @Override
     protected void onResume() {
         super.onResume();
-        picker1.onResume();
+        picker.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        picker1.onPause();
+        picker.onPause();
     }
 
 }
+
 
 
 
