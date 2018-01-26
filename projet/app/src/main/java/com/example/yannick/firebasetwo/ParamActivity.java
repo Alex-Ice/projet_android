445b42796a5b647949c36d9729579d0cb23643d2
nonock <yannick@ezvan.fr>
2018-01-26 14:19:51 +0100

    Ajout de dossier projet
    J'avais des bugs qui bloquaient le test de l'appli sur mon tel donc j'ai refais un projet et je vais te rajouter dessus sur firebase de suite

---
+++ projet/app/src/main/java/com/example/yannick/firebasetwo/ParamActivity.java
@@ -0,0 +1,77 @@
+package com.example.yannick.firebasetwo;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.IOException;
+
+public class ParamActivity extends AppCompatActivity {
+
+    private static final int CHOOSE_IMAGE = 101;
+    ImageView imageView;
+    EditText editText;
+
+    Uri uriProfileImage;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_param);
+
+        editText = (EditText) findViewById(R.id.editTextDisplayName);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showImageChooser();
+
+            }
+        });
+
+        findViewById(R.id.buttonSave).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CHOOSE_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null)
+        {
+            uriProfileImage = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uriProfileImage);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void showImageChooser()
+    {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Profile Image"),CHOOSE_IMAGE);
+    }
+
+}
